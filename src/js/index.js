367a7cd071b9ffb267100b4f1f654008d0214157
Henry <franzhenry46@gmail.com>
2023-05-10 16:43:17 -0300

    fazendo um prpojeto para 3-ADM

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+const namesStudents = [{
+    name: 'ciclano',
+    rm: 12648
+},
+{
+    name: 'deutrano',
+    rm: 12308
+},
+{
+    name: 'fulano',
+    rm: 12333
+}
+]
+
+document.getElementById('btn').addEventListener('click', (ev) => {
+    ev.preventDefault()
+    let name = document.getElementById('fullNameStudent').value
+    let rm = document.getElementById('RmStudent').value
+    if (name !== '' && rm !== '') {
+        render(name, rm)
+    }
+    else {
+        alert('informe os dados certos')
+    }
+
+})
+function render(name, rm) {
+    const person = namesStudents.find(ele => ele.name == name && ele.rm == rm)
+    console.log(person)
+}
