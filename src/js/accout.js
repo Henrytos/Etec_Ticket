79b3f3e7da864a6b6058425ecd390281290bd6c0
Henry <franzhenry46@gmail.com>
2023-05-21 17:10:56 -0300

    ajustes

--- src/js/accout.js
+++ src/js/accout.js
@@ -1,117 +1,117 @@
 export const accout = [
     {
-        rm: "12317", nameStudent: "ALVIN ELI ALBUQUERQUE LIMA ", password: null
+        rm: "12317", nameStudent: "ALVIN ELI ALBUQUERQUE LIMA", password: null
     },
     {
-        rm: "12326", nameStudent: "ANA CLARA AQUILES MIRANDA ", password: null
+        rm: "12326", nameStudent: "ANA CLARA AQUILES MIRANDA", password: null
     },
     {
-        rm: "12331", nameStudent: "ARTHUR DA SILVA PIRES LOPES ", password: null
+        rm: "12331", nameStudent: "ARTHUR DA SILVA PIRES LOPES", password: null
     },
     {
-        rm: "12871", nameStudent: "BARBARA MORAES DOS SANTOS ", password: null
+        rm: "12871", nameStudent: "BARBARA MORAES DOS SANTOS", password: null
     },
     {
-        rm: "12312", nameStudent: "DENISE TOKORO DA SILVA ", password: null
+        rm: "12312", nameStudent: "DENISE TOKORO DA SILVA", password: null
     },
     {
-        rm: "12329", nameStudent: "EMYLE GABRIELY RIBEIRO RAMOS ", password: null
+        rm: "12329", nameStudent: "EMYLE GABRIELY RIBEIRO RAMOS", password: null
     },
     {
-        rm: "12309", nameStudent: "FELIPE AUGUSTO DE MORAIS ", password: null
+        rm: "12309", nameStudent: "FELIPE AUGUSTO DE MORAIS", password: null
     },
     {
-        rm: "12322", nameStudent: "GABRIEL CAVALCANTE VALE DE SOUSA ", password: null
+        rm: "12322", nameStudent: "GABRIEL CAVALCANTE VALE DE SOUSA", password: null
     },
     {
-        rm: "12765", nameStudent: "GABRIELLY NAMIE HIDANI SANTOS ", password: null
+        rm: "12765", nameStudent: "GABRIELLY NAMIE HIDANI SANTOS", password: null
     },
     {
-        rm: "12310", nameStudent: "GIOVANNA CALDEIRINI OLIVEIRA ", password: null
+        rm: "12310", nameStudent: "GIOVANNA CALDEIRINI OLIVEIRA", password: null
     },
     {
-        rm: "12332", nameStudent: "GLAYCIANNE ARAUJO DA COSTA ", password: null
+        rm: "12332", nameStudent: "GLAYCIANNE ARAUJO DA COSTA", password: null
     },
     {
-        rm: "12318", nameStudent: "GUILHERME DE CAMPOS OLIVEIRA ", password: null
+        rm: "12318", nameStudent: "GUILHERME DE CAMPOS OLIVEIRA", password: null
     },
 
     {
-        rm: "12328", nameStudent: "GUSTAVO HENRIQUE SILVA DOS SANTOS MAGNO ", password: null
+        rm: "12328", nameStudent: "GUSTAVO HENRIQUE SILVA DOS SANTOS MAGNO", password: null
     },
     {
-        rm: "12648", nameStudent: "HENRY FRANZ RAMOS ARCAYA ", password: null
+        rm: "12648", nameStudent: "HENRY FRANZ RAMOS ARCAYA", password: null
     },
     {
-        rm: "12308", nameStudent: "IANO SANTOS CUMA ", password: null
+        rm: "12308", nameStudent: "IANO SANTOS CUMA", password: null
     },
     {
-        rm: "12649", nameStudent: "JAQUELINE TEIXEIRA DA SILVA ", password: null
+        rm: "12649", nameStudent: "JAQUELINE TEIXEIRA DA SILVA", password: null
     },
     {
-        rm: "12730", nameStudent: "JOAO VITOR DE ALMEIDA DA SILVA ", password: null
+        rm: "12730", nameStudent: "JOAO VITOR DE ALMEIDA DA SILVA", password: null
     },
     {
-        rm: "12307", nameStudent: "KAIQUE BRITO DOS SANTOS ", password: null
+        rm: "12307", nameStudent: "KAIQUE BRITO DOS SANTOS", password: null
     },
     {
-        rm: "12737", nameStudent: "KAUA ALEXANDRE FERREIRA DE OLIVEIRA LOPES ", password: null
+        rm: "12737", nameStudent: "KAUA ALEXANDRE FERREIRA DE OLIVEIRA LOPES", password: null
     },
     {
-        rm: "12647", nameStudent: "KEMILY DE CARVALHO ALVES ", password: null
+        rm: "12647", nameStudent: "KEMILY DE CARVALHO ALVES", password: null
     },
     {
-        rm: "12735", nameStudent: "LARISSA VITORIA DE OLIVEIRA MATOS ", password: null
+        rm: "12735", nameStudent: "LARISSA VITORIA DE OLIVEIRA MATOS", password: null
     },
     {
-        rm: "12650", nameStudent: "LEONARDO CARRARA DELFANTE ", password: null
+        rm: "12650", nameStudent: "LEONARDO CARRARA DELFANTE", password: null
     },
     {
-        rm: "12314", nameStudent: "LIVIAN ALVES ", password: null
+        rm: "12314", nameStudent: "LIVIAN ALVES", password: null
     },
     {
-        rm: "12321", nameStudent: "LUCAS DE SOUZA BRASSOLOTTO ", password: null
+        rm: "12321", nameStudent: "LUCAS DE SOUZA BRASSOLOTTO", password: null
     },
     {
-        rm: "12799", nameStudent: "LUCAS DIMITRY DIAS BUENO ", password: null
+        rm: "12799", nameStudent: "LUCAS DIMITRY DIAS BUENO", password: null
     },
     {
-        rm: "12324", nameStudent: "LUCAS VINICIUS ALVES DA SILVA ", password: null
+        rm: "12324", nameStudent: "LUCAS VINICIUS ALVES DA SILVA", password: null
     },
     {
-        rm: "12654", nameStudent: "LUCIANO GALIMBERTTI ", password: null
+        rm: "12654", nameStudent: "LUCIANO GALIMBERTTI", password: null
     },
     {
-        rm: "12313", nameStudent: "MARIANA XAVIER QUISPE ", password: null
+        rm: "12313", nameStudent: "MARIANA XAVIER QUISPE", password: null
     },
     {
-        rm: "12331", nameStudent: "MATHEUS FIDELES DA SILVA ", password: null
+        rm: "12331", nameStudent: "MATHEUS FIDELES DA SILVA", password: null
     },
     {
-        rm: "12333", nameStudent: "NATHALIA BRITO CARDOSO ", password: null
+        rm: "12333", nameStudent: "NATHALIA BRITO CARDOSO", password: null
     },
     {
-        rm: "12327", nameStudent: "NICKOLAS RODOLFO MANOEL DE ARAUJO ", password: null
+        rm: "12327", nameStudent: "NICKOLAS RODOLFO MANOEL DE ARAUJO", password: null
     },
     {
-        rm: "12320", nameStudent: "PEDRO HENRIQUE FERNANDES MELO ", password: null
+        rm: "12320", nameStudent: "PEDRO HENRIQUE FERNANDES MELO", password: null
     },
     {
-        rm: "12323", nameStudent: "RAISSA MELO DE ANDRADE ", password: null
+        rm: "12323", nameStudent: "RAISSA MELO DE ANDRADE", password: null
     },
     {
-        rm: "12763", nameStudent: "SOPHIA COBIANCHI FIGUEIREDO ", password: null
+        rm: "12763", nameStudent: "SOPHIA COBIANCHI FIGUEIREDO", password: null
     },
     {
-        rm: "12316", nameStudent: "THAYANE OLIVEIRA DA SILVA ", password: null
+        rm: "12316", nameStudent: "THAYANE OLIVEIRA DA SILVA", password: null
     },
     {
-        rm: "12736", nameStudent: "THAYSSA NATHIARY DOS SANTOS SILVA ", password: null
+        rm: "12736", nameStudent: "THAYSSA NATHIARY DOS SANTOS SILVA", password: null
     },
     {
-        rm: "12886", nameStudent: "VINICIUS FAJAN PEREIRA ", password: null
+        rm: "12886", nameStudent: "VINICIUS FAJAN PEREIRA", password: null
     },
     {
-        rm: "12330", nameStudent: "VITORIA DE SOUZA MATRONIANI ", password: null
+        rm: "12330", nameStudent: "VITORIA DE SOUZA MATRONIANI", password: null
     },
-    { rm: "12319", nameStudent: "YGOR TAVARES GOMES", password: null }]
+    { rm: "12319", nameStudent: "YGOR TAVARES GOME", password: null }]
