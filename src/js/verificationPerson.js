e2894ab6fd1b967ad1237c95c8cff78d4d09bc8d
Henry <franzhenry46@gmail.com>
2023-05-23 15:09:55 -0300

    direncionado pessoas a pagina de login depois de cadastro e resumindo códigos js com modulação

--- src/js/verificationPerson.js
+++ src/js/verificationPerson.js
@@ -1,13 +1,19 @@
 import { accout } from "./persons/accout.js"
-import { validatePassword } from "./validatePassword.js"
 /*Verificando se nome e Rm estão atrelados */
 export function verificationPerson(name, rm, password, password2) {
 
     if (password === password2 && (accout.find(person => person.nameStudent.toLowerCase() == name.toLowerCase() && person.rm == rm) ?? false) && validatePassword(password)) {
         alert("Login valido")
-
+        window.location.href = './login.html'
 
     } else {
         alert('Nome ou RM não atrelados ou senha menor que 8 caracteres ou senhas diferentes')
+        document.querySelectorAll('input').forEach(ele => ele.value = '')
     }
+}
+/*validar senha */
+export function validatePassword(senha) {
+    // pelo menos 8 caracteres, uma letra maiúscula, uma letra minúscula e um número
+    var senhaRegex = /^(.{8,})$/
+    return senhaRegex.test(senha);
 }
